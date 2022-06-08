f34025e057d6453bb6431473bd205d9d1dfe743a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-08 13:57:34 -0400

    Update and rename 99-recover-binary-search-tree.py to 99-recover-binary-search-tree_O(n).py

--- 99-recover-binary-search-tree/99-recover-binary-search-tree_O(n).py
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree_O(n).py
@@ -10,19 +10,24 @@ class Solution(object):
         :type root: TreeNode
         :rtype: None Do not return anything, modify root in-place instead.
         """
+        # store all nodes walked through inorder traversal
         nodes = []
         self.helper(root, nodes)
+        # from start to end find 1st node which next val is less than current node val
         i = 0
         while nodes[i].val < nodes[i+1].val:
             i += 1
         first = nodes[i]
+        # from end to start find 2nd node which previous val is greater than current node val
         j = len(nodes) - 1
         while nodes[j].val > nodes[j-1].val:
             j -= 1
         second = nodes[j]
+        # 1st and 2nd are the mistake ones, swap their values
         first.val, second.val = second.val, first.val
         return root
-        
+    
+    # Inorder traversal    
     def helper(self, node, res):
         if node:
             self.helper(node.left, res)
