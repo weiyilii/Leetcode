cb9dc82d95fc640b39ec3fa5ce15ec54ac90fc8f
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-09 18:31:25 -0400

    Time: 77 ms (36.45%), Space: 21.1 MB (72.89%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -18,20 +18,27 @@ class Solution(object):
         self.dfs(root)
         return self.res
     
+    # Basic mechanism: Inorder traversal
+    # Use prev to record current root's previous node and compare their values
     def dfs(self, root):
         if root:
             self.dfs(root.left)
             
+            # count frequency of each value
             if root.val == self.prev.val:
                 self.count += 1
             else:
                 self.count = 1
-            
+            # compare count with max_count
+            # if > max_count, current value is mode
+            # update max_count and res
             if self.count > self.max_count:
                 self.res = [root.val]
                 self.max_count = self.count
+            # if equal, append current value to res
             elif self.count == self.max_count:
                 self.res.append(root.val)
+            # update prev to current root and move on
             self.prev = root
             
             self.dfs(root.right)
