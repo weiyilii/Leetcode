caf2e41e473ad1a1399dc99a3b3ea5f2abb7a132
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-13 21:33:29 -0400

    Time: 412 ms (56.73%), Space: 29.4 MB (29.44%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        max_len = 0       
+        max_len = 0 
+        # time complexity = O(1) using set
         nums_set = set(nums)
         for num in nums_set:
+            # start sequence from num when num-1 not in set
             if num - 1 not in nums_set:
                 cur_len = 1
                 cur_num = num
+                # expand sequence
                 while cur_num + 1 in nums_set:
                     cur_len += 1
                     cur_num += 1
