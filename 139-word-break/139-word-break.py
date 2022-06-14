09d792a9fe72d290d6ef03d056122f0fbfccd649
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-13 22:04:10 -0400

    Time: 27 ms (78.43%), Space: 13.8 MB (27.99%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -9,6 +9,9 @@ class Solution(object):
         for i in range(len(s)):
             for w in wordDict:
                 w_len = len(w)
+                # res[i] is true when: 
+                # 1. w matches sub str ending at ith element of res
+                # 2. res is true at the left of that sub str or it's 1st word
                 if w == s[i-w_len+1 : i+1] and (res[i-w_len] or i-w_len == -1):
                     res[i] = True
         return res[-1]
