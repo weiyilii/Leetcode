82242525bba42c627c2213eeacb25b69b29beb8a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-13 22:34:35 -0400

    Time: 24 ms (46.15%), Space: 13.5 MB (39.50%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -6,13 +6,14 @@ class Solution(object):
         :rtype: List[str]
         """
         n = len(s)
+        words = set(wordDict)
         
         def dfs(start, path):
             if start == n:
                 res.append(path[1:])
             for i in range(start, n):
                 w = s[start:i+1]
-                if w in wordDict:
+                if w in words:
                     dfs(i+1, path + " " + w)
         
         res = []
