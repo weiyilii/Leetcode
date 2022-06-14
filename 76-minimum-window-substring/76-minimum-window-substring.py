c9eb6e08a1bae4c35c2fe1a7ec849c51272f9b06
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-13 21:04:32 -0400

    Time: 114 ms (65.96%), Space: 14.2 MB (69.73%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -11,17 +11,24 @@ class Solution(object):
         
         char_counts = collections.Counter(t)
         char_seen = collections.defaultdict(int)
+        # char_len is the number of unique characters in t
         char_len = len(char_counts)
+        # found measures how many unique characters from t the sliding window includes
+        # when char_len == found, current sliding window is desirable
         found = 0
         left, right = 0, 0
         min_len = 10**5 + 1
         res = (0, 0)
-        
+        # use right pointer to expand to right
+        # when sliding window is desirable, increment left pointer to contract window
         while right < m:
+            # operate one character at the right pointer each time
             char = s[right]
             char_seen[char] += 1
             if char in char_counts and char_seen[char] == char_counts[char]:
                 found += 1
+                
+            # contract current window until it is no longer desirable
             while left <= right and found == char_len:
                 if right - left + 1 < min_len:
                     min_len = right - left + 1
@@ -31,6 +38,7 @@ class Solution(object):
                 if char in char_counts and char_seen[char] < char_counts[char]:
                     found -= 1
                 left += 1
+            # when current window is no longer desirable, expand using right pointer   
             right += 1
         
         if min_len == 10**5 + 1:
