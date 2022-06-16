c79a10e4e59c535e8b4058c0ef0dbe1c1b0a30a0
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-15 21:42:50 -0400

    Time: 144 ms (77.94%), Space: 18.1 MB (21.20%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -8,14 +8,21 @@ class Solution(object):
         """
         if endWord not in wordList:
             return 0
+        # changemap records wordList's variations changing one letter
+        # {*ot:["hot", "dot"], h*t:["hot"], ....}
         changemap = collections.defaultdict(list)
         n = len(endWord)
         for word in wordList:
             for i in range(n):
                 key = word[:i] + '*' + word[i+1:]
                 changemap[key].append(word)
+        # BFS: from current word, change one letter each time 
+        # and get words by mapping its root from changemap
+        # these words form the new layer
         q = collections.deque()
         q.append((beginWord, 1))
+        # a word might appear in new layer many times
+        # Use visited to keep track of visited word
         visited = collections.defaultdict(int)
         while q:
             qlen = len(q)
@@ -26,6 +33,7 @@ class Solution(object):
                 for j in range(n):
                     key = word[:j] + '*' + word[j+1:]
                     for item in changemap[key]:
+                        # Only when item is not visited, append it to visited and queue
                         if item not in visited:
                             visited[item] = level + 1
                             q.append((item, level + 1))
