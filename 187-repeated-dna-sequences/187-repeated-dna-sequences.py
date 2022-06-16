be7b1e424edf47f9f1d05c824eb2b69dc62b8a05
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-16 17:21:13 -0400

    Time: 60 ms (82.75%), Space: 31.5 MB (51.25%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -4,8 +4,12 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
-        counts = collections.defaultdict(int)
-        for i in range(len(s)-9):
-            counts[s[i:i+10]] += 1
-        res = [key for key, value in counts.items() if value > 1]
-        return res
+        sequence, repeated = set(), set()
+        
+        for i in range(len(s) - 9):
+            sub = s[i: i+10]
+            if sub in sequence:
+                repeated.add(sub)
+            sequence.add(sub)
+            
+        return list(repeated)
