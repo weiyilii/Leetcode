2c3acb5a778a31f786d785663fd512a8bd0a920c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-15 23:43:30 -0400

    Time: 24 ms (64.42%), Space: 13.7 MB (15.34%) - LeetHub

--- 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
+++ 166-fraction-to-recurring-decimal/166-fraction-to-recurring-decimal.py
@@ -9,7 +9,9 @@ class Solution(object):
         i, remainder = divmod(abs(numerator), abs(denominator))
         r = {}
         res = '.'
+        # r tracks all remainders, if new remainder has been in r, remainder will repeat
         while remainder > 0 and remainder not in r:
+            # len(res) will tell where to insert "("
             r[remainder] = len(res)
             n, remainder = divmod(remainder*10, abs(denominator))
             res += str(n)
