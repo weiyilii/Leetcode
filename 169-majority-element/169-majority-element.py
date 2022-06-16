b3420c73745d715bb9658b225653d60a7abaa5f7
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-16 16:53:47 -0400

    Time: 156 ms (69.24%), Space: 15.8 MB (14.94%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -4,6 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n = len(nums)
-        d = collections.Counter(nums)
-        return max(d, key=d.get)
+        candidate, count = nums[0], 1
+        for i in range(1, len(nums)):
+            if count == 0:
+                candidate, count = nums[i], 1
+            else:
+                count += 1 if candidate == nums[i] else -1
+        return candidate
