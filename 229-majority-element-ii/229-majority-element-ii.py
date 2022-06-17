cfccdb9894c8b55aca87ec72f1b1d5a4be8d4904
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-17 00:03:17 -0400

    Time: 584 ms (5.37%), Space: 14.6 MB (44.86%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.py
+++ 229-majority-element-ii/229-majority-element-ii.py
@@ -4,9 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = []
-        dic = collections.Counter(nums)
-        for num in dic:
-            if dic[num] > len(nums) // 3:
-                res.append(num)
+        candidate1, candidate2, count1, count2 = 0, 0, 0, 0
+        for num in nums:
+            print(candidate1, count1)
+            print(candidate2, count2)
+            if num == candidate1:
+                count1 += 1
+            elif num == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1, count1 = num, 1
+            elif count2 == 0:
+                candidate2, count2 = num, 1
+            else:
+                count1 -= 1
+                count2 -= 1
+        res = [n for n in set([candidate1, candidate2]) if nums.count(n) > len(nums) // 3]
         return res
