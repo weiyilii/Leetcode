8b6a329a3c940baef7c24c294c8b5037b0f6ebce
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-16 23:30:45 -0400

    Time: 733 ms (46.13%), Space: 21.7 MB (96.57%) - LeetHub

---
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        seen = set()
+        for i, num in enumerate(nums):
+            if num in seen:
+                return True
+            seen.add(num)
+            if len(seen) > k:
+                seen.remove(nums[i-k])
+        return False
