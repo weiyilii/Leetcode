a9547e2a4397289ac63f21c85cac51030dc5b194
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-17 20:02:14 -0400

    Time: 89 ms (95.00%), Space: 13.7 MB (15.45%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.py
+++ 264-ugly-number-ii/264-ugly-number-ii.py
@@ -7,6 +7,9 @@ class Solution(object):
         ugly = [1]
         i2, i3, i5 = 0, 0, 0
         while n > 1:
+            # i2, 3, 5 as 3 pointers track the indexes for multiples of 2, 3, 5
+            # once each is min and got appended to ugly number array
+            # it will move forward
             u2, u3, u5 = ugly[i2]*2, ugly[i3]*3, ugly[i5]*5
             umin = min(u2, u3, u5)
             ugly.append(umin)
