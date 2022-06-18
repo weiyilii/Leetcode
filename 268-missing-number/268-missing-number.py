f70d6adf3f7e3d7fa6fce4b72b7e11d225a90423
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-17 20:36:52 -0400

    Time: 121 ms (78.38%), Space: 14.7 MB (42.69%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,6 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # use index as hash, walk through nums
+        # for each num, modify nums[num]
+        # walk through again if num > 0, that means its index has not been visited
+        # index as a number is not in nums
+        # be careful about index=n and 0
+        # after modification, if no negative number
+        # that means index of 0 is missed in nums
         nums.append(1)
         
         for num in nums[:len(nums)-1]:
