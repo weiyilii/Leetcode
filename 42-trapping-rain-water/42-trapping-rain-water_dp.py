244a36fd7c2dca66826e1f433b70e1818fc6875b
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-20 17:54:41 -0400

    Update and rename 42-trapping-rain-water.py to 42-trapping-rain-water_dp.py

--- 42-trapping-rain-water/42-trapping-rain-water_dp.py
+++ 42-trapping-rain-water/42-trapping-rain-water_dp.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        # DP: use 2 lists traking left max and right max
+        # left max: max of all elements on the left side, right max is the same
+        # water every point can hold: min(left_max[i], right_max[i) - height[i]
         n = len(height)
         left_max, right_max = [0]*n, [0]*n
         left, right, res = 0, 0, 0
