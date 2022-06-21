d567e06235d1b59d8ccd2a278dc1cc727adfe5cf
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-21 17:15:51 -0400

    Update and rename 227-basic-calculator-ii.py to 227-basic-calculator-ii_stack.py

--- 227-basic-calculator-ii/227-basic-calculator-ii_stack.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii_stack.py
@@ -5,16 +5,21 @@ class Solution(object):
         :rtype: int
         """
         stack = []
+        # number is the newest number that has not been appended to stack or calculated
+        # op is the last operation we've seen
         number, op = 0, '+'
         for i in range(len(s)):
             char = s[i]
             if char.isdigit():
                 number = number*10 + int(char)
+            # if current char is operation, we need to evaluate last operation
             if (not char.isdigit() and char != ' ') or i == len(s)-1:
+                # if last op is + or -, no need to calculate for now, just append newest number
                 if op == '+':
                     stack.append(number)
                 elif op == '-':
                     stack.append(-number)
+                # if last op is *, calculate using stack.pop() and current number, then append it to stack
                 elif op == '*':
                     num1 = stack.pop()
                     stack.append(num1*number)
