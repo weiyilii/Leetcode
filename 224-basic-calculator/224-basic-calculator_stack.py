355085606665ed86c3f854944ff9308a49779edd
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-21 18:21:34 -0400

    Update and rename 224-basic-calculator.py to 224-basic-calculator_stack.py

--- 224-basic-calculator/224-basic-calculator_stack.py
+++ 224-basic-calculator/224-basic-calculator_stack.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # res: calculated from previous expressions
+        # number: newest number that is going to be operated
+        # sign: only +1 or -1 to decide plus or minus number
         res, number, sign = 0, 0, 1
         stack = []
         for i in range(len(s)):
@@ -18,10 +21,13 @@ class Solution(object):
                 res += number*sign
                 number = 0
                 sign = -1
+            # if meet (, append previous result and sign into stack, sign on top, redo a new calculation
             elif char == '(':
                 stack.append(res)
                 stack.append(sign)
                 res, sign = 0, 1
+            # if ), current res + sign*num is the result inside parenthese
+            # 1st stack.pop() gives sign of it, 2st stack.pop() gives old result
             elif char == ')':
                 res += number*sign
                 res = res*stack.pop() + stack.pop()
