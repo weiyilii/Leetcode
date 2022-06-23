e1fe914a0480070ec67353c7ae55b4bcdb155cda
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-23 16:33:46 -0400

    Time: 178 ms (36.18%), Space: 20.9 MB (11.40%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -4,27 +4,29 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        res, number, sign = 0, 0, 1
+        res, cur_num, sign = 0, 0, 1
         stack = []
         for i in range(len(s)):
             char = s[i]
             if char.isdigit():
-                number = number*10 + int(char)
-            elif char == '+':
-                res += number*sign
-                number = 0
-                sign = 1
-            elif char == '-':
-                res += number*sign
-                number = 0
-                sign = -1
-            elif char == '(':
-                stack.append(res)
-                stack.append(sign)
-                res, sign = 0, 1
-            elif char == ')':
-                res += number*sign
-                res = res*stack.pop() + stack.pop()
-                number = 0
-        res += number*sign
+                cur_num = cur_num*10 + int(char)
+            if (not char.isdigit() and char != ' ') or i == len(s)-1:
+                if char == '+':
+                    res += sign*cur_num
+                    cur_num = 0
+                    sign = 1
+                elif char == '-':
+                    res += sign*cur_num
+                    cur_num = 0
+                    sign = -1
+                elif char == '(':
+                    stack.append(res)
+                    stack.append(sign)
+                    res = 0
+                    sign = 1
+                elif char == ')':
+                    res += sign*cur_num
+                    res = res*stack.pop() + stack.pop()
+                    cur_num = 0
+        res += sign*cur_num
         return res
