7ab942c080cd45b6afb024e602bd43e30bb6cebb
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-27 00:31:23 -0400

    Update and rename 373-find-k-pairs-with-smallest-sums.py to 373-find-k-pairs-with-smallest-sums_heap.py

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums_heap.py
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums_heap.py
@@ -6,14 +6,19 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        # Heap
         from heapq import heappop, heappush
         res = []
         visited = {(0, 0)}
+        # maintain a heap h whose h[0] = (s,(n1, n2)) always has samllest s (nums1[n1]+nums2[n2])
         h = [(nums1[0]+nums2[0], (0, 0))]
         l1, l2 = len(nums1), len(nums2)
         while len(res) < k and h:
             n1, n2 = heappop(h)[1]
             res.append([nums1[n1], nums2[n2]])
+            # n1, n2 are both ascending
+            # smallest sum must be nums1[0]+nums2[0]
+            # next smallest sum can only be given by (n1+1, n2) or (n1, n2+1)
             new_n1 = n1+1 if n1 < l1-1 else n1
             new_n2 = n2+1 if n2 < l2-1 else n2
             if (new_n1, n2) not in visited:
