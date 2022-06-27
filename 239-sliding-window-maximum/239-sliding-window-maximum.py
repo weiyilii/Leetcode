bd3a80ba3728ebb46631e022b23a1fd28b8b0644
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-26 22:48:34 -0400

    Time: 2687 ms (31.06%), Space: 31 MB (34.30%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -5,14 +5,22 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # Max queue
+        # queue stores indexes of num not number itself
         q = collections.deque()
         res = []
         for i in range(len(nums)):
+            # remove elements from left that is lefter than sliding window
+            # worst case is only lefter by 1 so no need to use while
             if q and q[0] < i-k+1:
                 q.popleft()
+            # remain leftest element is the largest
+            # everytime an element is coming into the queue
+            # pop element which is less it from the tail
             while q and nums[q[-1]] < nums[i]:
                 q.pop()
             q.append(i)
+            # only when i has iterated >= k elements, start appending results
             if i + 1 >= k:
                 res.append(nums[q[0]])
         return res
