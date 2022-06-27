77bd9f16220880faf82285dbeeea2e862c607ae0
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-06-27 01:15:15 -0400

    Update and rename 378-kth-smallest-element-in-a-sorted-matrix.py to 378-kth-smallest-element-in-a-sorted-matrix_2.py

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix_2.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix_2.py
@@ -5,13 +5,17 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # Heap
+        # Find the kth smallest from n sorted arrays
         from heapq import heappush, heappop
         n = len(matrix)
         h = []
+        # Initiate the min heap with all the first element in each row
         for i in range(n):
             heappush(h, (matrix[i][0], (i, 0)))
         while k > 0:
             (res, (i, j)) = heappop(h)
+            # everytime pop an element, push the right element in the same row into heap if not out of list range
             if j < n-1:
                 heappush(h, (matrix[i][j+1], (i, j+1)))
             k -= 1
