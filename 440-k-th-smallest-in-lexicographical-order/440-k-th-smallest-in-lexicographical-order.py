ebc4da3fd367921a5c815a2c8a36b387f0e97686
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-02 01:03:35 -0400

    Time: 33 ms (100.00%), Space: 13.5 MB (100.00%) - LeetHub

---
+++ 440-k-th-smallest-in-lexicographical-order/440-k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        res = 1
+        k -= 1
+
+        while k > 0:
+            count = 0
+            interval = [res, res+1]
+            while interval[0] <= n:
+                count += min(n+1, interval[1]) - interval[0]
+                interval = [interval[0]*10, interval[1]*10]
+            if k >= count:
+                k -= count
+                res += 1
+            else:
+                k -= 1
+                res *= 10
+        return res
+                
