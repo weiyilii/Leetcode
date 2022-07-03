cc575f0c3a89d65efb7277617ec2349c13df0a39
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-03 00:20:44 -0400

    Time: 177 ms (80.38%), Space: 53.2 MB (39.24%) - LeetHub

---
+++ 648-replace-words/648-replace-words.py
@@ -0,0 +1,46 @@
+class TrieNode(object):
+    def __init__(self):
+        self.children = collections.defaultdict(TrieNode)
+        self.is_word = False
+        self.indx = None
+
+class Trie(object):
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insearch(self, i, word):
+        current = self.root
+        for letter in word:
+            current = current.children[letter]
+        current.is_word = True
+        current.indx = i
+
+class Solution(object):
+    def replaceWords(self, dictionary, sentence):
+        """
+        :type dictionary: List[str]
+        :type sentence: str
+        :rtype: str
+        """
+        trie = Trie()
+        for i, root in enumerate(dictionary):
+            trie.insearch(i, root)
+            
+        words = sentence.split(" ")
+        res = []
+        for word in words:
+            new = ""
+            current = trie.root
+            for letter in word:
+                current = current.children.get(letter)
+                if current is None:
+                    break
+                if current.is_word:
+                    new = dictionary[current.indx]
+                    break
+            if new == "":
+                res.append(word)
+            else:
+                res.append(new)
+        res = " ".join(res)
+        return res
