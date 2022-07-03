c047a1b6a3e5b4132d4ba590e8eed423c22a65af
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-02 23:49:52 -0400

    Time: 484 ms (68.98%), Space: 29.4 MB (25.13%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -5,16 +5,23 @@ class Solution(object):
         :rtype: List[str]
         """
         d = set(words)
+        memo = {}
         def dfs(word):
+            if word in memo:
+                return memo[word]
+            memo[word] = False
             for i in range(1, len(word)):
                 prefix = word[:i]
                 suffix = word[i:]
                 if prefix in d and suffix in d:
-                    return True
+                    memo[word] = True
+                    break
                 if prefix in d and dfs(suffix):
-                    return True
+                    memo[word] = True
+                    break
                 if suffix in d and dfs(prefix):
-                    return True
-            return False
+                    memo[word] = True
+                    break
+            return memo[word]
         res = [word for word in words if dfs(word)]
         return res
