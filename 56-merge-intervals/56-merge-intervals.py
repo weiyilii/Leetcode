3646d72cdcaa453379025cd15b2609a576ab35b3
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-05 00:08:26 -0400

    Time: 180 ms (45.14%), Space: 18.2 MB (52.38%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort(key = lambda interval: interval[0])
+        
+        res = []        
+        for interval in intervals:
+            if not res or res[-1][1] < interval[0]:
+                res.append(interval)
+            else:
+                res[-1][1] = max(res[-1][1], interval[1])
+        return res
