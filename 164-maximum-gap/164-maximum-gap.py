a1b3a3c557f60243e551e03a70cead4a99de8195
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-09 17:24:13 -0400

    Time: 2486 ms (13.17%), Space: 62 MB (5.99%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.py
+++ 164-maximum-gap/164-maximum-gap.py
@@ -1,27 +1,12 @@
-class Solution(object):
+class Solution:
     def maximumGap(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        low,  high = min(nums), max(nums)
-        n = len(nums)
-        
-        if n <= 2 or low == high:
-            return high-low
-        
-        buckets = collections.defaultdict(list)
+        lo, hi, n = min(nums), max(nums), len(nums)
+        if n <= 2 or hi == lo: return hi - lo
+        B = defaultdict(list)
         for num in nums:
-            if num == high:
-                buckets[n-2].append(num)
-            else:
-                key = (n-1)*(num-low)//(high-low)
-                buckets[key].append(num)
-        diff = []
-        for bucket in buckets.values():
-            if bucket:
-                diff.append([min(bucket), max(bucket)])
-        res = 0
-        for i in range(1, len(diff)):
-            res = max(res, diff[i][0]-diff[i-1][1])
-        return res
+            ind = n-2 if num == hi else (num - lo)*(n-1)//(hi-lo)
+            B[ind].append(num)
+            
+        cands = [[min(B[i]), max(B[i])] for i in range(n-1) if B[i]]
+        return max(y[0]-x[1] for x,y in zip(cands, cands[1:]))
+        
