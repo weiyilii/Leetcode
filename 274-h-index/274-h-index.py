dc540873031fd6cc2fadbce5abd51ef9229d51b7
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-10 21:35:48 -0400

    Time: 26 ms (83.72%), Space: 13.8 MB (41.47%) - LeetHub

---
+++ 274-h-index/274-h-index.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse = True)
+        h = 0
+        for i in range(len(citations)):
+            if citations[i] >= (i+1):
+                h += 1
+        return h
