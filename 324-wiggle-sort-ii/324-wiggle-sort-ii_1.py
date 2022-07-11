a8a3233ea98827ddbda7727bec7010d26879996f
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-10 23:26:51 -0400

    Update and rename 324-wiggle-sort-ii.py to 324-wiggle-sort-ii_1.py

--- 324-wiggle-sort-ii/324-wiggle-sort-ii_1.py
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii_1.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
+        # sort the array descendingly
+        # find mid and divide into left and right (mid is in right)
+        # left elements (large) go to index = 1, 3, 5, 7 ...
+        # right elements (small) go to index = 0, 2, 4, 6 ...
+        
         copy = sorted(nums, reverse = True)
         n = len(nums)
         m = n//2
