19b4556a1cf53c5a9c1964ed783b178a2f64533b
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-12 16:44:20 -0400

    Time: 3147 ms (46.60%), Space: 13.6 MB (81.33%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        lis = [1]*n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    lis[i] = max(lis[i], 1+lis[j])
+        return max(lis)
