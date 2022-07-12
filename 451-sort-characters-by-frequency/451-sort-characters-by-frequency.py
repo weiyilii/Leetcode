fc954f058b86e9940a1435b462ad40f23137b17c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-12 19:17:38 -0400

    Time: 138 ms (22.86%), Space: 16.4 MB (48.25%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def frequencySort(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        count = collections.Counter(s)
+        freq = count.values()
+        high, low = max(freq), min(freq)
+        buckets = [[] for i in range(high - low + 1)]
+        res = ''
+        
+        for letter in count:
+            n = count[letter]
+            buckets[high - n] += [letter*n]
+            
+        for bucket in buckets:
+            res += ''.join(bucket)
+        return res
