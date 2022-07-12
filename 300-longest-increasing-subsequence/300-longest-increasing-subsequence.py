9b753049d659cffd2af6d1ba94ba4338ec1d0ac2
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-12 17:34:58 -0400

    Time: 83 ms (88.99%), Space: 13.7 MB (81.33%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        n = len(nums)
+        tail = []
+        size = 0
+        for num in nums:
+            if not tail or num > tail[-1]:
+                tail.append(num)
+                size += 1
+            else:
+                l, r = 0, size
+                while l < r:
+                    m = l + (r - l) // 2
+                    if tail[m] < num:
+                        l = m + 1
+                    else:
+                        r = m
+                tail[l] = num
+        return size
