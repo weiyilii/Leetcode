0e05600f656f8a435f894b9960c68fbca47f0a6e
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-14 16:28:48 -0400

    Time: 68 ms (85.50%), Space: 17.5 MB (20.54%) - LeetHub

--- 539-minimum-time-difference/539-minimum-time-difference.py
+++ 539-minimum-time-difference/539-minimum-time-difference.py
@@ -4,16 +4,11 @@ class Solution(object):
         :type timePoints: List[str]
         :rtype: int
         """
-        def timeDiff(t1, t2):
-            t1 = map(int, t1.split(':'))
-            t2 = map(int, t2.split(':'))
-            diff1 = (t2[0] - t1[0])*60 + t2[1] - t1[1]
-            diff2 = t1[0]*60 + t1[1] + (24 - t2[0])*60 - t2[1]
-            return min(diff1, diff2)
+        def toMin(t):
+            t = map(int, t.split(":"))
+            return t[0]*60 + t[1]
         
-        timePoints.sort()
-        res = 24*60
-        for i in range(0, len(timePoints)-1):
-            res = min(res, timeDiff(timePoints[i], timePoints[i+1]))
-        res = min(res, timeDiff(timePoints[0], timePoints[len(timePoints)-1]))
-        return res
+        t = map(toMin, timePoints)
+        t.sort()
+        
+        return min((y - x)%(24*60) for x, y in zip(t, t[1:] + t[:1]))
