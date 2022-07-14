173fb7675cb0682f4376622313fddd24a3b52e9d
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-14 14:27:47 -0400

    Update and rename 502-ipo.py to 502-ipo_1.py

--- 502-ipo/502-ipo_1.py
+++ 502-ipo/502-ipo_1.py
@@ -12,6 +12,10 @@ class Solution(object):
         i = 0
         projects = sorted(zip(profits, capital), key = lambda x: x[1])
         for _ in range(k):
+            # push all projects whose capital is <= current w into h
+            # next time start from next i
+            # everytime, it may push lots of projects into h
+            # but for each time only pop one out with largest profits
             while i < len(projects) and w >= projects[i][1]:
                 heappush(h, -projects[i][0])
                 i += 1
