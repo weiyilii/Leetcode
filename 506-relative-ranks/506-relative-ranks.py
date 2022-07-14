f945acb3fd1d5545ed5903c376812e1b4ade0a88
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-14 14:39:15 -0400

    Time: 110 ms (47.34%), Space: 14.3 MB (80.68%) - LeetHub

--- 506-relative-ranks/506-relative-ranks.py
+++ 506-relative-ranks/506-relative-ranks.py
@@ -5,18 +5,17 @@ class Solution(object):
         :rtype: List[str]
         """
         n = len(score)
-        res = [[] for _ in range(n)]
         rank = []
         for i, s in enumerate(score):
             rank.append((i, s))
         rank.sort(key = lambda x: x[1], reverse = True)
         for j in range(n):
             if j == 0:
-                res[rank[j][0]] = "Gold Medal"
+                score[rank[j][0]] = "Gold Medal"
             elif j == 1:
-                res[rank[j][0]] = "Silver Medal"
+                score[rank[j][0]] = "Silver Medal"
             elif j == 2:
-                res[rank[j][0]] = "Bronze Medal"
+                score[rank[j][0]] = "Bronze Medal"
             else:
-                res[rank[j][0]] = str(j + 1)
-        return res
+                score[rank[j][0]] = str(j + 1)
+        return score
