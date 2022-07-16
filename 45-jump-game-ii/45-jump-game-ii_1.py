2a9dafe8ba05ecd6db9ab322b259e8748f5afcb4
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-15 21:32:37 -0400

    Update and rename 45-jump-game-ii.py to 45-jump-game-ii_1.py

--- 45-jump-game-ii/45-jump-game-ii_1.py
+++ 45-jump-game-ii/45-jump-game-ii_1.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # BFS idea, Greedy
+        # find possible max reach point in each level
         n = len(nums)
         
         if n <= 1:
@@ -11,8 +13,11 @@ class Solution(object):
         
         i, level = 0, 0
         cur_max, next_max = 0, 0
+        # i: start of each level
+        # cur_max: end of each level, also the possible max reach point in each level
         while i <= cur_max:
             for j in range(i, cur_max+1):
+                # find possible max reach point in next level
                 next_max = max(next_max, j + nums[j])
                 if next_max >= n-1:
                     return level + 1
