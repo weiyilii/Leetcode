b680a1238ecbc5f7f52a141bbcf769fc81cff226
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-19 19:17:53 -0400

    Update and rename 611-valid-triangle-number.py to 611-valid-triangle-number_1.py

--- 611-valid-triangle-number/611-valid-triangle-number_1.py
+++ 611-valid-triangle-number/611-valid-triangle-number_1.py
@@ -6,6 +6,12 @@ class Solution(object):
         """
         nums.sort()
         res = 0
+        # i < j < k, nums[i] < nums[j] < nums[k]
+        # just make sure nums[i] + nums[j] > nums[k], then it is a valid triangle
+        # iterate k from 2 to len-1
+        # start with i = 0, j = k-1
+        # if it is valid then all j-i pairs are valid: update res and j = j-1
+        # else: update i = i+1
         for k in range(2, len(nums)):
             i, j = 0, k-1
             while i < j:
