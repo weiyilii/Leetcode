ad521b348dfc9cf0182885990396181b2f301811
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-21 01:38:54 -0400

    Update and rename 646-maximum-length-of-pair-chain.py to 646-maximum-length-of-pair-chain_greedy.py

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain_greedy.py
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain_greedy.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type pairs: List[List[int]]
         :rtype: int
         """
+        # sort pairs by right
+        # the less right is, the longer the chain can be
+        
         pairs.sort(key = lambda x: x[1])
         cur, res = float('-inf'), 0
         
