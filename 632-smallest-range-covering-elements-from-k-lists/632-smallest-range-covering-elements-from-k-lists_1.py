7146c1e18184d58a6fc33ca9962e84bbcc12f284
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-23 02:04:53 -0400

    Update and rename 632-smallest-range-covering-elements-from-k-lists.py to 632-smallest-range-covering-elements-from-k-lists_1.py

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists_1.py
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists_1.py
@@ -4,10 +4,18 @@ class Solution(object):
         :type nums: List[List[int]]
         :rtype: List[int]
         """
+        # Heap
+        # Possible results must be selecting one from each num
+        # Maintain a heap that always holds one elem from each num
+        # get left by heappop 
+        # after pop, push that element's next one (next_elem) to heap
+        # right = max(current max, next_elem)
+        
         from heapq import heapify, heappush, heappop
+        # Initialize heap with all first elements from all num
         h = [(num[0], i, 0) for i, num in enumerate(nums)]
         heapify(h)
-        
+        # Initialize right with the max among all first elements (each first is min in their own num)
         right = max(num[0] for num in nums)
         ans = [float('-inf'), float('inf')] 
         
