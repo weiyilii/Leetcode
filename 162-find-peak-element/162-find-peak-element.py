5655ccc896eafbb10df8fc369dbe6054a46709f2
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-27 20:28:03 -0400

    Time: 33 ms (84.87%), Space: 14.2 MB (6.92%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def dfs(left, right, nums):
+        def dfs(left, right):
             if left == right:
                 return left
             mid = left + (right - left)//2
             if mid + 1 <= right and nums[mid] < nums[mid + 1]:
-                return dfs(mid + 1, right, nums)
+                return dfs(mid + 1, right)
             elif mid - 1 >= left and nums[mid] < nums[mid - 1]:
-                return dfs(left, mid, nums)
+                return dfs(left, mid)
             else:
                 return mid
-        res = dfs(0, len(nums) - 1, nums)
+        res = dfs(0, len(nums) - 1)
         return res
             
