693e87d05652be611b477fe1eb35bce1ae0d460d
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-07-30 00:24:05 -0400

    Time: 8721 ms (5.02%), Space: 36.5 MB (41.62%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -14,7 +14,6 @@ class Solution(object):
             helper(nums, sort, mid+1, b)
             
             for i in range(a, mid+1):
-                #print(nums[i], sort[mid+1:b+1])
                 low, high = mid+1, b
                 while low <= high:
                     middle = low + (high - low) // 2
@@ -23,8 +22,7 @@ class Solution(object):
                     else:
                         high = middle - 1
                 count[i] += low - mid - 1
-                #print(nums[i], sort[mid+1:b+1], mid, high, count)
-            
+                
             sort[a:b+1] = sorted(sort[a:b+1])
             
         n = len(nums)
