ed46321d9814834e1ed7e05fd07a5efdea324108
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-02 00:04:31 -0400

    Time: 9044 ms (10.55%), Space: 15.1 MB (48.00%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.py
@@ -1,28 +1,36 @@
 from sortedcontainers import SortedList
-class Solution:
-    def maxSumSubmatrix(self, matrix: List[List[int]], k: int) -> int:
+class Solution(object):
+    def maxSumSubmatrix(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
         m, n = len(matrix), len(matrix[0])
-        ans = -math.inf
+        res = float('-inf')
         for r1 in range(m):
-            arr = [0] * n  # arr[i] is sum(matrix[r1][c]...matrix[r2][c])
+            array = [0]*n
             for r2 in range(r1, m):
-                for c in range(n): arr[c] += matrix[r2][c]
-                ans = max(ans, self.maxSumSubAarray(arr, n, k))
-        return ans
-
-    def maxSumSubAarray(self, arr, n, k):
-        right = 0  # PrefixSum so far
-        seen = SortedList([0])
-        ans = -math.inf
+                for c in range(n):
+                    array[c] += matrix[r2][c]
+                res = max(res, self.maxSumSubarray(array, k))
+        return res        
+    
+    def maxSumSubarray(self, array, k):
+        ans = float('-inf')
+        n = len(array)
+        presum = SortedList([0])
+        right = 0
         for i in range(n):
-            right += arr[i]
-            left = self.ceiling(seen, right - k)  # right - left <= k -> left >= right - k
+            right += array[i]
+            left = self.findLeft(presum, right - k)
             if left != None:
                 ans = max(ans, right - left)
-            seen.add(right)
+            presum.add(right)
         return ans
-
-    def ceiling(self, sortedList, key):  # O(logN)
-        idx = sortedList.bisect_left(key)
-        if idx < len(sortedList): return sortedList[idx]
+    
+    def findLeft(self, array, key):
+        idx = array.bisect_left(key)
+        if idx < len(array) and idx >= 0:
+            return array[idx]
         return None
