6a07b582beeaaa17693288bf3df3eb0cf4983175
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-02 22:09:03 -0400

    Time: 2419 ms (74.19%), Space: 21.4 MB (83.19%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reversePairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def helper(nums, left, right):
+            if left >= right:
+                return 0
+            
+            mid = left + (right - left) // 2
+            count = helper(nums, left, mid) + helper(nums, mid + 1, right)
+            
+            j = mid + 1
+            for i in range(left, mid + 1):        
+                while j <= right and nums[i] > 2*nums[j]:
+                    j += 1
+                count += j - mid - 1
+            
+            nums[left:right+1] = sorted(nums[left:right+1])
+                    
+            return count
+        
+        return helper(nums, 0, len(nums) - 1)
+                
