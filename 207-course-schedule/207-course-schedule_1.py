80453155bd228eef57edcc789f72c7f4901ecf5c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-07 14:07:58 -0400

    Update and rename 207-course-schedule.py to 207-course-schedule_1.py

--- 207-course-schedule/207-course-schedule_1.py
+++ 207-course-schedule/207-course-schedule_1.py
@@ -6,6 +6,7 @@ class Solution(object):
         :rtype: bool
         """
         visited = [0 for i in range(numCourses)]
+        # Create graph as adjacency list
         graph = [[] for i in range(numCourses)]
         for [x, y] in prerequisites:
             graph[y].append(x)
@@ -16,6 +17,9 @@ class Solution(object):
         return True
     
     def dfs(self, i, graph, visited):
+        # Mark node being visited as -1
+        # Mark node has been visited as 1
+        # in each dfs, if a node is -1, that means it is being visited before, a cycle is found
         if visited[i] == -1:
             return False
         if visited[i] == 1:
@@ -24,5 +28,6 @@ class Solution(object):
         for j in graph[i]:
             if not self.dfs(j, graph, visited):
                 return False
+        # After finishing dfs this node, mark it as 1
         visited[i] = 1
         return True
