00e7a582037ab8f6adcf19b26e0ece485037ffeb
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-09 23:39:34 -0400

    Time: 34 ms (93.66%), Space: 18.3 MB (22.01%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def rob(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        dp = dict()
+        res = self.dfs(root, dp)
+        return res
+    
+    def dfs(self, root, dp):
+        if root is None:
+            return 0
+        if root in dp:
+            return dp[root]
+        
+        val = 0
+        if root.left:
+            val += self.dfs(root.left.left, dp) + self.dfs(root.left.right, dp)
+        if root.right:
+            val += self.dfs(root.right.left, dp) + self.dfs(root.right.right, dp)
+            
+        dp[root] = max(root.val+val, self.dfs(root.left, dp)+self.dfs(root.right, dp))
+        return dp[root]
