3c884f50c29d1d7df54939d079a6d115ff9d5bee
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-13 22:31:19 -0400

    Time: 77 ms (30.97%), Space: 14.5 MB (18.58%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
+        graph = collections.defaultdict(set)
+        
+        def dfs(source, target):
+            if source not in seen:
+                seen.add(source)
+                if source == target:
+                    return True
+                for neighbor in graph[source]:
+                    if dfs(neighbor, target):
+                        return True
+                return False
+        
+        for source, target in edges:
+            seen = set()
+            #if source in graph and target in graph and dfs(source, target):
+            if dfs(source, target):
+                return [source, target]
+            graph[source].add(target)
+            graph[target].add(source)
