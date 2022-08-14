c9b1b9390f2c6727f1034a5b88707a7fb725e3cd
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-14 18:07:07 -0400

    Time: 87 ms (71.67%), Space: 14.5 MB (63.96%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.py
@@ -11,16 +11,13 @@ class Solution(object):
         :type root2: TreeNode
         :rtype: TreeNode
         """
-        def dfs(root1, root2):
-            if not root1 and not root2:
-                return
-            if not root1:
-                return root2
-            if not root2:
-                return root1
-            root1.val += root2.val
-            root1.left = dfs(root1.left, root2.left)
-            root1.right = dfs(root1.right, root2.right)
+        if not root1 and not root2:
+            return
+        if not root1:
+            return root2
+        if not root2:
             return root1
-        
-        return dfs(root1, root2)
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+        return root1
