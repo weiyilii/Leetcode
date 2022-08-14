3c8eb87451c4a81305750561a735051e379869c9
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-13 22:42:39 -0400

    Update and rename 684-redundant-connection.py to 684-redundant-connection_1.py

--- 684-redundant-connection/684-redundant-connection_1.py
+++ 684-redundant-connection/684-redundant-connection_1.py
@@ -4,11 +4,17 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: List[int]
         """
+        # DFS
+        # For each edge (source to target), use dfs to see if there is already a path from source to target using previously visited nodes in graph
+        # If so, then this edge is duplicate
         graph = collections.defaultdict(set)
         
         def dfs(source, target):
+            # When use dfs, may visit a node twice because u -> v and v -> u
+            # Only when not seen source, continue checking
             if source not in seen:
                 seen.add(source)
+                # if source == target, means we have searched target, there is a path
                 if source == target:
                     return True
                 for neighbor in graph[source]:
@@ -18,6 +24,7 @@ class Solution(object):
         
         for source, target in edges:
             seen = set()
+            # if dfs(source, target) is also acceptable but add if source in graph and target in graph can save some time
             if source in graph and target in graph and dfs(source, target):
                 return [source, target]
             graph[source].add(target)
