d7b03a3df540a9fe7344b37b2b05a925ec807df8
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-16 17:11:52 -0400

    Create  637-average-of-levels-in-binary-tree_2.py

---
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree_2.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        # BFS
+        from collections import deque
+        q = deque([root])
+        res = []
+        while q:
+            l = len(q)
+            lsum = 0
+            for _ in range(l):
+                node = q.popleft()
+                if node:
+                    lsum += node.val
+                    if node.left:
+                        q.append(node.left)
+                    if node.right:
+                        q.append(node.right)
+            res.append(float(lsum)/l)
+        return res
