cab4938a90c0aa3f020194bbdd2e609f4897b7a4
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-20 15:49:19 -0400

    Update and rename 90-subsets-ii.py to 90-subsets-ii_2.py

--- 90-subsets-ii/90-subsets-ii_2.py
+++ 90-subsets-ii/90-subsets-ii_2.py
@@ -4,12 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # Iterative, DP idea
+        # Use previous subsets. Add new elements to each previous subset and form a new subset
+        # cur stores last added items
         res, cur = [[]], []
         nums.sort()
         
         for i in range(len(nums)):
+            # if nums[i] == nums[i-1], only append nums[i] to subsets whose last item is nums[i-1] which are in cur
             if i > 0 and nums[i] == nums[i-1]:
                 cur = [item + [nums[i]] for item in cur]
+            # if nums[i] != nums[i-1], append nums[i] to all previous subsets
             else:
                 cur = [item + [nums[i]] for item in res]
             res += cur
