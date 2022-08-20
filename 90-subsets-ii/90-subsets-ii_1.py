8b04f65f9b6276405f4b3688d760425415230417
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-20 15:30:12 -0400

    Update and rename 90-subsets-ii.py to 90-subsets-ii_1.py

--- 90-subsets-ii/90-subsets-ii_1.py
+++ 90-subsets-ii/90-subsets-ii_1.py
@@ -4,11 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # DFS
         res = []
         nums.sort()
         
         def dfs(path, prev):
             res.append(path)
+            # unique means for nums[prev+1:], keys are unique elements, value is the first index of this key element
             unique = {}
             for i in range(prev+1, len(nums)):
                 if nums[i] not in unique:
