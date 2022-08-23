d4f98185f7eda8f5864ba6c74e35824ae02cbfe4
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-23 19:00:39 -0400

    Update and rename 32-longest-valid-parentheses.py to 32-longest-valid-parentheses_2.py

--- 32-longest-valid-parentheses/32-longest-valid-parentheses_2.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses_2.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        # Stack
         stack = [-1]
         res = 0
         # keep the stack top element is the index of element right previous to valid parentheses
@@ -11,9 +12,12 @@ class Solution(object):
             if s[i] == "(":
                 stack.append(i)
             else:
+                # everytime meet ), pop stack
+                # if stack becomes null, that means meet more ) than (, current i is invalid, append it to stack
                 stack.pop()
                 if not stack:
                     stack.append(i)
+                # if stack not null, means till now, meet ( >= ), still valid, stack[-1] is the left element of the valid parenthese
                 else:
                     res = max(res, i - stack[-1])
         return res
