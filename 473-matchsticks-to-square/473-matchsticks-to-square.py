4af3053d64eb6e976fe9be63d4023be2b085ab50
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-22 20:05:52 -0400

    Time: 3163 ms (41.24%), Space: 583.2 MB (6.10%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.py
+++ 473-matchsticks-to-square/473-matchsticks-to-square.py
@@ -1,20 +1,21 @@
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        value = sum(matchsticks)
-        if value < 4:
+        s = sum(matchsticks)
+        if s < 4 or s % 4 != 0:
             return False
-        if value % 4 != 0:
-            return False
-        edge = value // 4
-        matchsticks.sort(reverse=True)
+        matchsticks.sort(reverse = True)
+        p = s // 4
+        
         @cache
-        def findedges(l1, l2, l3, l4, i):
-            nonlocal edge
-            if l1 == l2 == l3 == l4 == edge:
-                return True
+        def dfs(i, l1, l2, l3, l4):
+            if i == len(matchsticks) - 1:
+                if l1 == l2 == l3 == p:
+                    return True
+                return False
             if i > len(matchsticks) - 1:
                 return False
-            if l1 > edge or l2 > edge or l3 > edge or l4 > edge:
+            if l1 > p or l2 > p or l3 > p or l4 > p:
                 return False
-            return findedges(l1 + matchsticks[i], l2, l3, l4, i + 1) or findedges(l1, l2 + matchsticks[i] , l3, l4, i + 1) or findedges(l1, l2, l3 + matchsticks[i], l4, i + 1) or findedges(l1, l2, l3, l4 + matchsticks[i] , i + 1)
-        return findedges(0, 0, 0, 0, 0)
+            return dfs(i+1, l1 + matchsticks[i+1], l2, l3, l4) or dfs(i+1, l1, l2 + matchsticks[i+1], l3, l4) or dfs(i+1, l1, l2, l3 + matchsticks[i+1], l4) or dfs(i+1, l1, l2, l3, l4 + matchsticks[i+1])
+        
+        return dfs(-1, 0, 0, 0, 0)
