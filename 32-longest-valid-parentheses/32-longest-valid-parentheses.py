0528db922bed54a0b7d289810fa39cd4c01dfa1f
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-23 18:52:14 -0400

    Time: 79 ms (16.46%), Space: 13.9 MB (85.37%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -4,15 +4,26 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        left, right = 0, 0
+        l = len(s)
         res = 0
-        stack = [-1]
-        for i in range(len(s)):
+        for i in range(l):
             if s[i] == "(":
-                stack.append(i)
+                left += 1
             else:
-                stack.pop()
-                if not stack:
-                    stack.append(i)
-                else:
-                    res = max(res, i - stack[-1])
+                right += 1
+            if left == right:
+                res = max(res, left + right)
+            if right > left:
+                left, right = 0, 0
+        left, right = 0, 0
+        for i in range(l-1, -1, -1):
+            if s[i] == "(":
+                left += 1
+            else:
+                right += 1
+            if left == right:
+                res = max(res, left + right)
+            if left > right:
+                left, right = 0, 0
         return res
