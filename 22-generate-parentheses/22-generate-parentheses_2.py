c87ec9c4aea2fbe1cb978a89a274fac5a4e559bd
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-23 18:13:31 -0400

    Update and rename 22-generate-parentheses.py to 22-generate-parentheses_2.py

--- 22-generate-parentheses/22-generate-parentheses_2.py
+++ 22-generate-parentheses/22-generate-parentheses_2.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        # DP
+        # dp[i] stores all results having i pairs of parentheses
+        # dp[i] = [(dp[0])dp[i-1], (dp[1])dp[i-2],..., (dp[i-1])dp[0]]
         dp = [[] for _ in range(n+1)]
         dp[0] = [""]
         for i in range(n):
