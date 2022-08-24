8a5e087c838b5d4291ae0f30432990b5962aed16
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-23 20:48:38 -0400

    Time: 10815 ms (7.46%), Space: 14.7 MB (17.62%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -8,15 +8,11 @@ class Solution(object):
         if l <= 1:
             return 0
         
-        i, level = 0, 0
-        cur_max, next_max = 0, 0
+        dp = [10001]*l
+        dp[-1] = 0
         
-        while i <= cur_max:
-            for j in range(i, cur_max + 1):
-                next_max = max(next_max, j + nums[j])
-                if next_max >= l-1:
-                    return level + 1
-            i = cur_max + 1
-            level += 1
-            cur_max = next_max
-        return -1
+        for i in range(l-2, -1, -1):
+            for j in range(1, nums[i]+1):
+                if i + j <= l-1:
+                    dp[i] = min(dp[i], dp[i+j] + 1)
+        return dp[0]
