5076fde7d985eb5690221c0c276255fbabb24afe
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-24 15:27:19 -0400

    Time: 215 ms (93.69%), Space: 22.3 MB (16.50%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.py
+++ 85-maximal-rectangle/85-maximal-rectangle.py
@@ -4,23 +4,27 @@ class Solution(object):
         :type matrix: List[List[str]]
         :rtype: int
         """
-        if not matrix or not matrix[0]:
-            return 0
         m, n = len(matrix), len(matrix[0])
-        height = [0]*(n+1)
+        dp = [0]*n
         res = 0
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == "1":
-                    height[j] += 1
+                    dp[j] += 1
                 else:
-                    height[j] = 0
-            # for each row, create a histogram, then apply #84
-            stack = [-1]
-            for j in range(n+1):
-                while height[j] < height[stack[-1]]:
-                    h = height[stack.pop()]
-                    w = j - stack[-1] -1
-                    res = max(res, h*w)
-                stack.append(j)
+                    dp[j] = 0
+            res = max(res, self.hist(dp))
         return res
+        
+    def hist(self, heights):
+        res = 0
+        stack = [-1]
+        heights.append(0)
+        for i in range(len(heights)):
+            while heights[i] < heights[stack[-1]]:
+                h = heights[stack.pop()]
+                w = i - stack[-1] - 1
+                res = max(res, h*w)
+            stack.append(i)
+        return res
+        
