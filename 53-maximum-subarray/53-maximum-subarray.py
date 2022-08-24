ccde4c851c566d4617d2a95a13077e05224c1718
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-23 21:11:59 -0400

    Time: 1006 ms (31.25%), Space: 24.1 MB (99.80%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,15 +4,13 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        dp = [0]*len(nums)
-        dp[0] = nums[0]
-        res = dp[0]
-        
+        prev = nums[0]
+        res = prev
         for i in range(1, len(nums)):
-            if dp[i-1] >= 0:
-                dp[i] = nums[i] + dp[i-1]
+            if prev >= 0:
+                cur = nums[i] + prev
             else:
-                dp[i] = nums[i]
-            res = max(res, dp[i])
-        
+                cur = nums[i]
+            prev = cur
+            res = max(res, cur)
         return res
