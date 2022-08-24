53cef47dcd33fb8ee3835d759e8ba919e734f56c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-24 14:41:54 -0400

    Update and rename 72-edit-distance.py to 72-edit-distance_1.py

--- 72-edit-distance/72-edit-distance_1.py
+++ 72-edit-distance/72-edit-distance_1.py
@@ -5,9 +5,11 @@ class Solution(object):
         :type word2: str
         :rtype: int
         """
+        # DP
         word1 = " " + word1
         word2 = " " + word2
         l1, l2 = len(word1), len(word2)
+        # dp[i][j] = minimal number of operations needed to convert word1[:i+1] to word[:j+1]
         dp = [[0]*l2 for _ in range(l1)]
         for i in range(1, l1):
             dp[i][0] = i
@@ -18,5 +20,7 @@ class Solution(object):
                 if word1[i] == word2[j]:
                     dp[i][j] = dp[i-1][j-1]
                 else:
+                    # dp[i-1][j-1] + 1: replace word1[i] with word2[j]; dp[i][j-1] + 1: insert word2[j] to the index i of word1;
+                    # dp[i-1][j] + 1: delete word1[i]
                     dp[i][j] = min(dp[i-1][j-1], dp[i][j-1], dp[i-1][j]) + 1
         return dp[-1][-1]
