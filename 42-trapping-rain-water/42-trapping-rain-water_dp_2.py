d8c1634e47632b5ae2fd9f934450d1191f68a50c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 17:42:36 -0400

    Update and rename 42-trapping-rain-water.py to 42-trapping-rain-water_dp_2.py

--- 42-trapping-rain-water/42-trapping-rain-water_dp_2.py
+++ 42-trapping-rain-water/42-trapping-rain-water_dp_2.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        # DP idea with 2 pointers
         l = len(height)
         left_max, right_max = 0, 0
         left, right, res = 0, l - 1, 0
         while left <= right:
             left_max = max(left_max, height[left])
             right_max = max(right_max, height[right])
+            # if height[left] < height[right]: area relies on left_max, update left
             if height[left] < height[right]:
                 res += left_max - height[left]
                 left += 1
