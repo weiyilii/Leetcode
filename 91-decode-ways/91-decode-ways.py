515e660ebb26500ba73ae38644839490d1e9c3e0
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 15:57:14 -0400

    Time: 28 ms (61.87%), Space: 13.4 MB (75.07%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -10,14 +10,12 @@ class Solution(object):
         prev1, prev2, cur = 1, 1, 1
         for i in range(1, len(s)):
             cur = 0
-            num = int(s[i-1:i+1])
-            if s[i] == "0":
-                if num > 0 and num <= 26:
-                    cur += prev1
-            else:
+            num1 = int(s[i])
+            num2 = int(s[i-1:i+1])
+            if num1 > 0:
                 cur += prev2
-                if num >= 10 and num <= 26:
-                    cur += prev1
+            if num2 >= 10 and num2 <= 26:
+                cur += prev1
             prev1 = prev2
             prev2 = cur
         
