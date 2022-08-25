06d22c113fb40ef75016d1505c3cc78ed40ec896
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 16:36:36 -0400

    Time: 166 ms (37.27%), Space: 15.1 MB (30.25%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -4,27 +4,15 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        # 2 pointers
-        # ensure there is a bar on the right which is higher than left_max
-        # water can hold will only depend on left_max and height[left]
-        left_max, right_max, res = 0, 0, 0
-        left, right = 0, len(height)-1
-        while left < right:
-            if height[left] < height[right]:
-                # worry: left_max too large?
-                # as we have moved from left_max to left
-                # this means sometime earlier when left=index(left_max),
-                # we have seen height[index(left_max)] < height[right]
-                # only then left pointer can be updated
-                if height[left] >= left_max:
-                    left_max = height[left]
-                else:
-                    res += left_max - height[left]
-                left += 1
-            else:
-                if height[right] >= right_max:
-                    right_max = height[right]
-                else:
-                    res += right_max - height[right]
-                right -= 1
+        res = 0
+        stack = []
+        for i in range(len(height)):
+            while len(stack) > 0 and stack and height[i] > height[stack[-1]]:
+                index = stack.pop()
+                if not stack:
+                    break
+                h = min(height[i], height[stack[-1]]) - height[index]
+                w = i - stack[-1] - 1
+                res += h*w
+            stack.append(i)
         return res
