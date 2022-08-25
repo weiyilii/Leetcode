f796c3e9bd4257087539e1308816a0fb8b8eafc1
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 15:54:03 -0400

    Time: 43 ms (17.43%), Space: 13.4 MB (75.07%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s[0] == "0":
+            return 0
+        
+        prev1, prev2, cur = 1, 1, 1
+        for i in range(1, len(s)):
+            cur = 0
+            num = int(s[i-1:i+1])
+            if s[i] == "0":
+                if num > 0 and num <= 26:
+                    cur += prev1
+            else:
+                cur += prev2
+                if num >= 10 and num <= 26:
+                    cur += prev1
+            prev1 = prev2
+            prev2 = cur
+        
+        return cur
