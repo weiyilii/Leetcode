74e39815835e465f71f3abc9bb04eb15c52a6c4c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 15:11:56 -0400

    Time: 120 ms (54.31%), Space: 14.6 MB (56.05%) - LeetHub

---
+++ 87-scramble-string/87-scramble-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        
+        cache = {}
+        
+        def dfs(s1, s2):
+            l = len(s1)
+            if (s1, s2) in cache:
+                return cache[(s1, s2)]
+            if sorted(s1) != sorted(s2):
+                cache[(s1, s2)] = False
+                return False
+            if s1 == s2:
+                cache[(s1, s2)] = True
+                return True
+            for i in range(1, l):
+                if (dfs(s1[:i], s2[:i]) and dfs(s1[i:], s2[i:])) or (dfs(s1[:i], s2[-i:]) and dfs(s1[i:], s2[:-i])):
+                    cache[(s1, s2)] = True
+                    return True
+            cache[(s1, s2)] = False
+            return False
+        
+        return dfs(s1, s2)
+                
