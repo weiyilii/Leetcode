40b3db6a63517336189b48ae49e42d317f5929b0
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-25 15:02:50 -0400

    Update and rename 87-scramble-string.py to 87-scramble-string_1.py

--- 87-scramble-string/87-scramble-string_1.py
+++ 87-scramble-string/87-scramble-string_1.py
@@ -1,11 +1,14 @@
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
 
+        # Recursive dfs
         @cache
         def dfs(s1, s2):
             l = len(s1)
+            # make sure s1 and s2 contain same counts of same letters, else False
             if sorted(s1) != sorted(s2):
                 return False
+            # if l < 4 like "abc", all the permutations are scrambles of each other
             if l < 4 or s1 == s2:
                 return True
             for i in range(1, l):
