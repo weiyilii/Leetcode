14f1f39f75ec98b3e0ab393979a0d2b51214a337
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-08-31 16:15:37 -0400

    Update 453-minimum-moves-to-equal-array-elements.py

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # After sorting, nums[0] is min, nums[-1] is max, increment all except nums[-1] by their difference
+        # then min is still nums[0], max becomes nums[-2], do the same increment
+        # until min == max
         nums.sort()
         pos = len(nums) - 1
         res = 0
