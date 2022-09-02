3b0b72f358d0dea82fd2232d9dff6eac2a4f338a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-02 15:15:38 -0400

    Time: 89 ms (7.14%), Space: 13.4 MB (90.48%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         res = ""
         for i in range(min(len(str1), len(str2))):
-            if str1[:i+1] != str2[:i+1]:
+            if str1[i] != str2[i]:
                 return res
             d = str1[:i+1]
             if len(str1) == str1.count(d)*(i+1) and len(str2) == str2.count(d)*(i+1):
