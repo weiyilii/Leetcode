4d8ae82a378feac8707033457e398e74944682e7
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-02 14:21:51 -0400

    Time: 1619 ms (29.80%), Space: 17 MB (28.97%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        l = len(nums)
+        if l < 3:
+            return 0
+        res = []
+        nums.sort()
+        i, prev = 0, nums[0] - 1
+        while i < l - 2:
+            if nums[i] != prev:
+                res += self.twoSum(nums, i + 1, -nums[i])
+                prev = nums[i]
+            i += 1
+        return res
+    
+    def twoSum(self, nums, i, target):
+        l = len(nums)
+        if i > l - 2:
+            return 0
+        res = []
+        left, right = i, l - 1
+        prev = nums[right] + 1
+        while left < right:
+            if nums[right] != prev and nums[left] + nums[right] == target:
+                res.append([nums[i - 1], nums[left], nums[right]])
+                prev = nums[right]
+                right -= 1
+            elif nums[left] + nums[right] < target:
+                left += 1
+            else:
+                right -= 1
+        return res
