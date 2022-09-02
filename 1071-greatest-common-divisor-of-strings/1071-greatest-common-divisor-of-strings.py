38bb80504bdbd81161584d3327ce340d3497d9b3
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-02 15:23:28 -0400

    Time: 37 ms (40.48%), Space: 13.5 MB (70.24%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -5,12 +5,12 @@ class Solution(object):
         :type str2: str
         :rtype: str
         """
-        res = ""
-        for i in range(min(len(str1), len(str2))):
-            if str1[i] != str2[i]:
-                return res
-            d = str1[:i+1]
-            if len(str1) % (i + 1) == 0 and len(str2) % (i + 1) == 0:
-                if len(str1) == str1.count(d)*(i+1) and len(str2) == str2.count(d)*(i+1):
-                    res = d
-        return res
+        if str1 + str2 != str2 + str1:
+            return ""
+        l = self.gcd(len(str1), len(str2))
+        return str1[:l]
+    
+    def gcd(self, a, b):
+        if b == 0:
+            return a
+        return self.gcd(b, a%b)
