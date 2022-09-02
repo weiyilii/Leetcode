538e64aeb99999e5756b4f6476802f03a26c2211
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-02 15:14:16 -0400

    Time: 132 ms (5.95%), Space: 13.8 MB (8.93%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        res = ""
+        for i in range(min(len(str1), len(str2))):
+            if str1[:i+1] != str2[:i+1]:
+                return res
+            d = str1[:i+1]
+            if len(str1) == str1.count(d)*(i+1) and len(str2) == str2.count(d)*(i+1):
+                res = d
+        return res
