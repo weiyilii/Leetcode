2ebb1ca88b745f519810e6a41830bc723f6a2c54
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-02 17:33:28 -0400

    Time: 175 ms (10.98%), Space: 14.7 MB (66.21%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -5,13 +5,13 @@ class Solution(object):
         :rtype: int
         """
         m, n = len(grid), len(grid[0])
-        dp = [[0]*n for _ in range(m)]
-        dp[0][0] = grid[0][0]
         for i in range(1, m):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
+            grid[i][0] += grid[i-1][0]
         for j in range(1, n):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
+            grid[0][j] += grid[0][j-1]
+        
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
-        return dp[-1][-1]
+                grid[i][j] += min(grid[i-1][j], grid[i][j-1])
+        
+        return grid[-1][-1]
