2abe9d708636c29a22885cd4bbd0ffc5dff09622
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-04 17:11:34 -0400

    Time: 198 ms (98.72%), Space: 23.7 MB (37.57%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -5,22 +5,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if sum(nums) < target:
+        left, ksum = 0, 0
+        res = float('inf')
+        for i in range(len(nums)):
+            ksum += nums[i]
+            while ksum >= target:
+                res = min(res, i - left + 1)
+                ksum -= nums[left]
+                left += 1
+        if res > len(nums):
             return 0
-        
-        left, right = 0, len(nums)
-        while left < right:
-            mid = left + (right - left)//2
-            kmax = ksum = sum(nums[:mid])
-            
-            for i in range(len(nums) - mid):
-                ksum += nums[i+mid] - nums[i]
-                kmax = max(kmax, ksum)
-            
-            if kmax == target:
-                return mid
-            elif kmax < target:
-                left = mid + 1
-            else:
-                right = mid
-        return right
+        else:
+            return res
