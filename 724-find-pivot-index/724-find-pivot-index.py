aa81938cdd357c5f6a34ec6b3790cde1a7f81410
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-04 15:35:23 -0400

    Time: 189 ms (60.19%), Space: 14.6 MB (39.71%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.py
+++ 724-find-pivot-index/724-find-pivot-index.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: int
         """
         l = len(nums)
-        left, right = [0]*l, [0]*l
-        left[0], right[-1] = 0, 0
-        for i in range(1, l):
-            left[i] = nums[i-1] + left[i-1]
-        for i in range(l-2, -1, -1):
-            right[i] = nums[i+1] + right[i+1]
+        left = 0
+        s = sum(nums)
         for i in range(l):
-            if left[i] == right[i]:
+            if left == s - left - nums[i]:
                 return i
+            left += nums[i]
         return -1
