623f7215bc5ba35066728517457c50de6ab8da4e
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-04 16:46:03 -0400

    Time: 1568 ms (92.31%), Space: 41.9 MB (38.46%) - LeetHub

--- 730-count-different-palindromic-subsequences/730-count-different-palindromic-subsequences.py
+++ 730-count-different-palindromic-subsequences/730-count-different-palindromic-subsequences.py
@@ -5,25 +5,25 @@ class Solution(object):
         :rtype: int
         """
         l = len(s)
-        dp = [[0 for _ in range(l)] for _ in range(l)]
+        dp = [[0]*l for _ in range(l)]
         for i in range(l):
             dp[i][i] = 1
         for distance in range(1, l):
             for i in range(l - distance):
                 j = i + distance
-                if s[i] == s[j]:
+                if s[i] != s[j]:
+                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1]
+                else:
                     low = i + 1
                     high = j - 1
-                    while low <= high and s[low] != s[j]:
+                    while low < j and s[low] != s[j]:
                         low += 1
-                    while low <= high and s[i] != s[high]:
+                    while high >= low and s[high] != s[i]:
                         high -= 1
                     if low > high:
                         dp[i][j] = dp[i+1][j-1]*2 + 2
                     elif low == high:
                         dp[i][j] = dp[i+1][j-1]*2 + 1
                     else:
-                        dp[i][j] = dp[i+1][j-1]*2 - dp[low+1][high-1]
-                else:
-                    dp[i][j] = dp[i][j-1] + dp[i+1][j] - dp[i+1][j-1]
+                        dp[i][j] = dp[i+1][j-1]*2 - dp[low + 1][high - 1]
         return dp[0][-1] % (10**9 + 7)
