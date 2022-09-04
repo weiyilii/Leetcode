22a78c4c0e051f60233cfb3195e2b28effab9341
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-04 18:31:15 -0400

    Time: 562 ms (13.75%), Space: 13.4 MB (76.25%) - LeetHub

---
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def knightProbability(self, n, k, row, column):
+        """
+        :type n: int
+        :type k: int
+        :type row: int
+        :type column: int
+        :rtype: float
+        """
+        lastdp = [[0]*n for _ in range(n)]
+        di = [-2, -1, 1, 2, 2, 1, -1, -2]
+        dj = [-1, -2, -2, -1, 1, 2, 2, 1]
+        lastdp[row][column] = 1
+        k -= 1
+        dp = lastdp
+        while k >= 0:
+            dp = [[0]*n for _ in range(n)]
+            for i in range(n):
+                for j in range(n):
+                    for t in range(8):
+                        x, y = i + di[t], j + dj[t]
+                        if x >= 0 and x < n and y >= 0 and y < n:
+                            dp[i][j] += lastdp[x][y]/8.0
+            lastdp = dp
+            k -= 1
+            
+        res = 0
+        for i in range(n):
+            for j in range(n):
+                res += dp[i][j]
+        return res
