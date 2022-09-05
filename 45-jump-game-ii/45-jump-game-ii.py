0571ec4cd785bbfaccd8688828c734533ae78d93
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-04 21:39:39 -0400

    Time: 187 ms (52.72%), Space: 14.3 MB (54.81%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l = len(nums)
-        if l <= 1:
-            return 0
-        
-        dp = [10001]*l
-        dp[-1] = 0
-        
-        for i in range(l-2, -1, -1):
-            for j in range(1, nums[i]+1):
-                if i + j <= l-1:
-                    dp[i] = min(dp[i], dp[i+j] + 1)
-        return dp[0]
+        jumps, currend, far = 0, 0, 0
+        for i in range(len(nums) - 1):
+            far = max(far, i + nums[i])
+            if i == currend:
+                jumps += 1
+                currend = far
+        return jumps
