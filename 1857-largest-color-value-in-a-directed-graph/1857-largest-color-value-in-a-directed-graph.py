9eb39c406f7d7230443cffaf9328071c3fd03301
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-06 19:04:39 -0400

    Time: 3318 ms (57.14%), Space: 83 MB (100.00%) - LeetHub

---
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.py
@@ -0,0 +1,48 @@
+class Solution(object):
+    def largestPathValue(self, colors, edges):
+        """
+        :type colors: str
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        from collections import deque
+        
+        graph = {}
+        n = len(colors)
+        degree = [0]*n
+        
+        for src, dst in edges:
+            if src in graph:
+                graph[src].append(dst)
+            else:
+                graph[src] = [dst]
+            degree[dst] += 1
+            
+        dp = [[0]*26 for _ in range(n)]
+        
+        q = deque([])
+        for i in range(n):
+            if degree[i] == 0:
+                q.append(i)
+                dp[i][ord(colors[i]) - ord("a")] = 1
+        
+        res, seen = 0, 0
+        
+        while q:
+            src = q.popleft()
+            seen += 1
+            value = max(dp[src])
+            res = max(res, value)
+            if src in graph:
+                for dst in graph[src]:
+                    for i in range(26):
+                        dp[dst][i] = max(dp[dst][i], dp[src][i])
+                    degree[dst] -= 1
+                    if degree[dst] == 0:
+                        dp[dst][ord(colors[dst]) - ord("a")] += 1
+                        q.append(dst)
+        #print(dp)            
+        if seen == n:
+            return res
+        else:
+            return -1
