ed2800a31799496b58ef00bcb109586dffa2aa21
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-07 13:04:25 -0400

    Time: 1605 ms (16.71%), Space: 53.6 MB (97.74%) - LeetHub

---
+++ 148-sort-list/148-sort-list.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        dummy = prev = ListNode(0, head)
+        fast, slow = head, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            prev = prev.next
+        prev.next = None
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        return self.mergeTwo(l1, l2)
+    
+    def mergeTwo(self, l1, l2):
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            x, y = l1.val, l2.val
+            if x <= y:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
