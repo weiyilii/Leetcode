1fa21484a69b6f52dab5f6687cc88883490ce827
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-07 15:40:56 -0400

    Time: 49 ms (31.49%), Space: 13.7 MB (28.74%) - LeetHub

---
+++ 139-word-break/139-word-break.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        l = len(s)
+        dp = [False]*l
+        for i in range(l):
+            for word in wordDict:
+                wlen = len(word)
+                if s[i - wlen + 1:i + 1] == word and (dp[i - wlen] or i - wlen == -1):
+                    dp[i] = True
+        return dp[-1]
