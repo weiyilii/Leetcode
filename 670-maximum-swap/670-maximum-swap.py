7b270f14c0441f36e80e977788f6c0093ee9b8a8
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-06 21:23:56 -0400

    Time: 33 ms (24.81%), Space: 13.4 MB (62.96%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.py
+++ 670-maximum-swap/670-maximum-swap.py
@@ -5,16 +5,14 @@ class Solution(object):
         :rtype: int
         """
         digits = [n for n in str(num)]
-        max_idx = len(digits) - 1
+        l = len(digits)
+        max_idx = l - 1
         x, y = 0, 0
         
-        for i in range(len(digits))[::-1]:
+        for i in range(l-1, -1, -1):
             if digits[i] > digits[max_idx]:
                 max_idx = i
             elif digits[i] < digits[max_idx]:
-                x = i
-                y = max_idx
-        
+                x, y = i, max_idx
         digits[x], digits[y] = digits[y], digits[x]
-        
-        return int(''.join(digits))
+        return int("".join(digits))
