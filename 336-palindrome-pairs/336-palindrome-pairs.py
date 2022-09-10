2d2c196df739d6b03d461e705beee31a45da30f7
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-09 22:03:11 -0400

    Time: 7393 ms (15.16%), Space: 26.4 MB (66.36%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.py
+++ 336-palindrome-pairs/336-palindrome-pairs.py
@@ -1,29 +1,21 @@
-class Solution(object):
-    def palindromePairs(self, words):
-        """
-        :type words: List[str]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
         def ispalindrome(s):
             return s == s[::-1]
         res = []
         dic = {word: i for i, word in enumerate(words)}
         for i in range(len(words)):
             word = words[i]
-            n = len(word)
-            # for a word "bot", pref = "", "b", "bo", "bot"
-            # if pref is palindrome ("", "b") (their surf: "bot", "ot")
-            # check if reversed surf in words ("tob", "to")
-            # if any in, reversed surf + word is a palindrome
-            for j in range(n+1):
-                pref = word[:j]
-                surf = word[j:]
-                if ispalindrome(pref):
-                    reverse = surf[::-1]
+            l = len(word)
+            for j in range(l+1):
+                prefix = word[:j]
+                suffix = word[j:]
+                if ispalindrome(prefix):
+                    reverse = suffix[::-1]
                     if reverse != word and reverse in dic:
                         res.append([dic[reverse], i])
-                if j != n and ispalindrome(surf):
-                    reverse = pref[::-1]
+                if j != l and ispalindrome(suffix):
+                    reverse = prefix[::-1]
                     if reverse != word and reverse in dic:
                         res.append([i, dic[reverse]])
         return res
