821d212a541ac2263fdab987d7aa9a0a35ea4873
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-12 12:18:17 -0400

    Time: 441 ms (12.72%), Space: 22.5 MB (28.39%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.py
+++ 386-lexicographical-numbers/386-lexicographical-numbers.py
@@ -1,19 +1,17 @@
-class Solution(object):
-    def lexicalOrder(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        def dfs(cur, res):
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        
+        def dfs(cur):
             if cur > n:
                 return
             res.append(cur)
             for i in range(10):
                 cur = cur*10 + i
-                dfs(cur, res)
+                dfs(cur)
                 cur = cur // 10
-                
-        res = []
+                        
         for i in range(1, min(10, n+1)):
-            dfs(i, res)
+            dfs(i)
+            
         return res
