c80ae36effba8767c102286cbaadd068ca3f8f5d
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-12 21:28:59 -0400

    Time: 5296 ms (5.01%), Space: 84.9 MB (5.00%) - LeetHub

---
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.py
@@ -0,0 +1,14 @@
+class Solution:
+    def closestCost(self, baseCosts: List[int], toppingCosts: List[int], target: int) -> int:
+        res = []
+        for base in baseCosts:
+            res.append(base)
+        for topping in toppingCosts:
+            l = len(res)
+            for i in range(l):
+                for j in [1, 2]:
+                    res.append(res[i] + j*topping)
+                    
+        res.sort(key = lambda x: (abs(x - target), x))
+        return res[0]
+        
