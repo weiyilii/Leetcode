8a235c7747a8d30d5b3db33044f4d7ecc88c85ce
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-14 09:24:45 -0400

    Time: 511 ms (43.79%), Space: 14.3 MB (86.51%) - LeetHub

---
+++ 490-the-maze/490-the-maze.py
@@ -0,0 +1,19 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        from collections import deque
+        m, n = len(maze), len(maze[0])
+        q = deque([(start[0], start[1])])
+        visited = [[False]*n for _ in range(m)]
+        while q:
+            i, j = q.popleft()
+            if i == destination[0] and j == destination[1]:
+                return True
+            for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                newi, newj = i, j
+                while 0 <= newi + di < m and 0 <= newj + dj < n and maze[newi + di][newj + dj] == 0:
+                    newi += di
+                    newj += dj
+                if not visited[newi][newj]:
+                    q.append((newi, newj))
+                    visited[newi][newj] = True
+        return False
