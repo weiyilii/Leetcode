62aaa6ab3bf2763d679b64398756b039d74e69a1
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-14 11:22:49 -0400

    Time: 105 ms (56.78%), Space: 15.6 MB (16.47%) - LeetHub

---
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.py
@@ -0,0 +1,32 @@
+class Solution:
+    def addBoldTag(self, s: str, words: List[str]) -> str:
+        intervals = []
+        for word in words:
+            wl = len(word)
+            index = s.find(word)
+            while index != -1:
+                intervals.append([index, index + wl])
+                index += 1
+                index = s.find(word, index)
+        if len(intervals) == 0:
+            return s
+        intervals.sort(key = lambda x: (x[0], x[1]))
+        merge = [intervals[0]]
+        for i in range(1, len(intervals)):
+            if merge[-1][1] >= intervals[i][0]:
+                merge[-1][1] = max(merge[-1][1], intervals[i][1])
+            else:
+                merge.append(intervals[i])
+
+        res = ""
+        prev = 0
+        for start, end in merge:
+            res += s[prev:start]
+            res += "<b>"
+            res += s[start:end]
+            res += "</b>"
+            prev = end
+        if prev < len(s):
+            res += s[prev:]
+        
+        return res
