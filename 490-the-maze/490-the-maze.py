8717112ef21c8d3201a3c0fa2aa482cb5100abff
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-14 01:03:18 -0400

    Time: 860 ms (14.13%), Space: 16.1 MB (26.93%) - LeetHub

---
+++ 490-the-maze/490-the-maze.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m, n = len(maze), len(maze[0])
+        visited = [[False]*n for _ in range(m)]
+        
+        def dfs(i, j):
+            if visited[i][j]:
+                return False
+            if i == destination[0] and j == destination[1]:
+                return True
+            visited[i][j] = True
+            for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                newi, newj = i, j
+                while 0 <= newi + di < m and 0 <= newj + dj < n and maze[newi + di][newj + dj] == 0:
+                    newi += di
+                    newj += dj
+                if dfs(newi, newj):
+                    return True
+            return False
+        
+        return dfs(start[0], start[1])
