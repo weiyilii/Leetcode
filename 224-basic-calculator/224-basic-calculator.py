f49e098d7ca2f19a9bcd5fa69265cbb799b7d2e6
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-14 20:42:33 -0400

    Time: 220 ms (22.10%), Space: 15.4 MB (76.30%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,32 +1,30 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        res, cur_num, sign = 0, 0, 1
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, number, sign = 0, 0, 1
+        i = 0
         stack = []
-        for i in range(len(s)):
-            char = s[i]
-            if char.isdigit():
-                cur_num = cur_num*10 + int(char)
-            if (not char.isdigit() and char != ' ') or i == len(s)-1:
-                if char == '+':
-                    res += sign*cur_num
-                    cur_num = 0
-                    sign = 1
-                elif char == '-':
-                    res += sign*cur_num
-                    cur_num = 0
-                    sign = -1
-                elif char == '(':
-                    stack.append(res)
-                    stack.append(sign)
-                    res = 0
-                    sign = 1
-                elif char == ')':
-                    res += sign*cur_num
-                    res = res*stack.pop() + stack.pop()
-                    cur_num = 0
-        res += sign*cur_num
+        while i < len(s):
+            c = s[i]
+            if c.isdigit():
+                number = number*10 + int(s[i])
+            if c == "+":
+                res += number*sign
+                number = 0
+                sign = 1
+            if c == "-":
+                res += number*sign
+                number = 0
+                sign = -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign = 1
+                res = 0
+            elif c == ")":
+                res += number*sign
+                res = res*stack.pop() + stack.pop()
+                number = 0
+            i += 1
+        res += number*sign
         return res
+            
