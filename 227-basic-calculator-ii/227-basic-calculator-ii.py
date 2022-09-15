f424d02de2c1498bcf2e7d6c351f4aa1247b48e1
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-14 21:31:04 -0400

    Time: 90 ms (91.14%), Space: 15.6 MB (52.67%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,27 +1,33 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def calculate(self, s: str) -> int:
+        number, op = 0, "+"
         stack = []
-        res, cur_num, last_num, op = 0, 0, 0, '+'
         for i in range(len(s)):
-            char = s[i]
-            if char.isdigit():
-                cur_num = cur_num*10 + int(char)
-            if (not char.isdigit() and char != ' ') or i == len(s)-1:
-                if op == '+':
-                    res += last_num
-                    last_num = cur_num
-                elif op == '-':
-                    res += last_num
-                    last_num = -cur_num
-                elif op == '*':
-                    last_num = last_num*cur_num
-                elif op == '/':
-                    last_num = int(float(last_num)/cur_num)
-                op = char
-                cur_num = 0
-        res += last_num
-        return res
+            c = s[i]
+            if c.isdigit():
+                number = number*10 + int(c)
+            if c in ("+", "-", "*", "/"):
+                if op == "+":
+                    stack.append(number)
+                if op == "-":
+                    stack.append(-number)
+                if op == "*":
+                    n = stack.pop()
+                    stack.append(n*number)
+                if op == "/":
+                    n = stack.pop()
+                    stack.append(int(n/number))
+                number = 0
+                op = c
+        if op == "+":
+            stack.append(number)
+        if op == "-":
+            stack.append(-number)
+        if op == "*":
+            n = stack.pop()
+            stack.append(n*number)
+        if op == "/":
+            n = stack.pop()
+            stack.append(int(n/number)) 
+        
+        return sum(stack)
