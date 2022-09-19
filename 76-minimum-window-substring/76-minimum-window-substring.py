57114d0add71e9885350a006e5f11af4bc65255c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-19 13:45:30 -0400

    Time: 223 ms (37.16%), Space: 14.8 MB (36.72%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,47 +1,29 @@
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         m, n = len(s), len(t)
         if m < n:
             return ""
-        
-        char_counts = collections.Counter(t)
-        char_seen = collections.defaultdict(int)
-        # char_len is the number of unique characters in t
-        char_len = len(char_counts)
-        # found measures how many unique characters from t the sliding window includes
-        # when char_len == found, current sliding window is desirable
-        found = 0
         left, right = 0, 0
-        min_len = 10**5 + 1
-        res = (0, 0)
-        # use right pointer to expand to right
-        # when sliding window is desirable, increment left pointer to contract window
+        start, l = 0, float('inf')
+        needs = collections.Counter(t)
+        window = collections.defaultdict(int)
+        valid = 0
         while right < m:
-            # operate one character at the right pointer each time
-            char = s[right]
-            char_seen[char] += 1
-            if char in char_counts and char_seen[char] == char_counts[char]:
-                found += 1
-                
-            # contract current window until it is no longer desirable
-            while left <= right and found == char_len:
-                if right - left + 1 < min_len:
-                    min_len = right - left + 1
-                    res = (left, right)
-                char = s[left]
-                char_seen[char] -= 1
-                if char in char_counts and char_seen[char] < char_counts[char]:
-                    found -= 1
-                left += 1
-            # when current window is no longer desirable, expand using right pointer   
+            c = s[right]
             right += 1
-        
-        if min_len == 10**5 + 1:
-            return ""
-        else:
-            return s[res[0]: res[1]+1]
+            if c in needs:
+                window[c] += 1
+                if needs[c] == window[c]:
+                    valid += 1
+                
+            while valid == len(needs):
+                if right - left < l:
+                    start = left
+                    l = right - left
+                d = s[left]
+                left += 1
+                if d in needs:
+                    if window[d] == needs[d]:
+                        valid -= 1
+                    window[d] -= 1
+        return "" if l > m else s[start:start + l]
