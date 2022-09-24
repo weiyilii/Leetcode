89af65e8a85f55d6ddf8fafe7e7960cfc93d412c
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-24 17:39:01 -0400

    Time: 178 ms (79.84%), Space: 53.9 MB (9.87%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -3,25 +3,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        for i in [0, len(board)-1]:
-            for j in range(len(board[0])):
-                self.dfs(board, i, j)
+        m, n = len(board), len(board[0])
         
-        for i in range(len(board)):
-            for j in [0, len(board[0])-1]:
-                self.dfs(board, i, j)
-                
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'
-                elif board[i][j] == '.':
-                    board[i][j] = 'O'
-    
-    def dfs(self, board, i, j):
-        if 0 <= i < len(board) and 0 <= j < len(board[0]) and board[i][j] == 'O':
-            board[i][j] = '.'
-            self.dfs(board, i-1, j)
-            self.dfs(board, i+1, j)
-            self.dfs(board, i, j-1)
-            self.dfs(board, i, j+1)
+        def dfs(i, j, char):
+            if i < 0 or i >= m or j < 0 or j >= n:
+                return
+            if board[i][j] == "O":
+                board[i][j] = char
+                dfs(i-1, j, char)
+                dfs(i+1, j, char)
+                dfs(i, j-1, char)
+                dfs(i, j+1, char)
+        
+        for i in [0, m-1]:
+            for j in range(n):
+                if board[i][j] == "O":
+                    dfs(i, j, "#")   
+        for j in [0, n-1]:
+            for i in range(1, m-1):
+                if board[i][j] == "O":
+                    dfs(i, j, "#")
+        
+        for i in range(1, m-1):
+            for j in range(1, n-1):
+                if board[i][j] == "O":
+                    dfs(i, j, "X")
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == "#":
+                    board[i][j] = "O"
