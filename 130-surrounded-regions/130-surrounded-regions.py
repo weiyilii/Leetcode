c469ec8a5ec4e78cc65547455c9b1f947d188d0d
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-24 17:43:35 -0400

    Time: 138 ms (97.18%), Space: 15.9 MB (72.55%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.py
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -5,31 +5,26 @@ class Solution:
         """
         m, n = len(board), len(board[0])
         
-        def dfs(i, j, char):
+        def dfs(i, j):
             if i < 0 or i >= m or j < 0 or j >= n:
                 return
             if board[i][j] == "O":
-                board[i][j] = char
-                dfs(i-1, j, char)
-                dfs(i+1, j, char)
-                dfs(i, j-1, char)
-                dfs(i, j+1, char)
+                board[i][j] = "#"
+                dfs(i-1, j)
+                dfs(i+1, j)
+                dfs(i, j-1)
+                dfs(i, j+1)
         
         for i in [0, m-1]:
             for j in range(n):
-                if board[i][j] == "O":
-                    dfs(i, j, "#")   
+                dfs(i, j)   
         for j in [0, n-1]:
             for i in range(1, m-1):
-                if board[i][j] == "O":
-                    dfs(i, j, "#")
-        
-        for i in range(1, m-1):
-            for j in range(1, n-1):
-                if board[i][j] == "O":
-                    dfs(i, j, "X")
+                dfs(i, j)
         
         for i in range(m):
             for j in range(n):
                 if board[i][j] == "#":
                     board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
