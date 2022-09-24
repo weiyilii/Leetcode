331bf609ad5b91565380035618f5c9130888822e
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-23 22:34:11 -0400

    Time: 1581 ms (26.65%), Space: 20 MB (94.82%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,34 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        from collections import deque
+        
+        graph = collections.defaultdict(set)
+        for [a, b] in dislikes:
+            graph[a].add(b)
+            graph[b].add(a)
+        
+        visited = [False]*(n + 1)
+        color = [False]*(n + 1)
+        self.bp = True
+        
+        def bfs(i):
+            visited[i] = True
+            q = deque()
+            q.append(i)
+            while q:
+                u = q.popleft()
+                for v in graph[u]:
+                    if visited[v]:
+                        if color[u] == color[v]:
+                            self.bp = False
+                            return
+                    else:
+                        color[v] = False if color[u] else True
+                        visited[v] = True
+                        q.append(v)
+        
+        for i in range(1, n+1):
+            if not visited[i]:
+                bfs(i)
+        
+        return self.bp
