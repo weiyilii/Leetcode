75ba95ebeb00ff99457629c302b23402362bd606
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-23 22:24:36 -0400

    Time: 1903 ms (11.49%), Space: 23.3 MB (9.78%) - LeetHub

---
+++ 886-possible-bipartition/886-possible-bipartition.py
@@ -0,0 +1,27 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        graph = collections.defaultdict(set)
+        for [a, b] in dislikes:
+            graph[a].add(b)
+            graph[b].add(a)
+            
+        visited = [False]*n
+        color = [False]*n
+        self.bp = True
+        
+        def dfs(i):
+            visited[i-1] = True
+            for j in graph[i]:
+                if visited[j-1]:
+                    if color[i-1] == color[j-1]:
+                        self.bp = False
+                        return
+                else:
+                    color[j-1] = False if color[i-1] else True
+                    dfs(j)
+        
+        for i in range(1, n+1):
+            if not visited[i-1]:
+                dfs(i)
+        
+        return self.bp
