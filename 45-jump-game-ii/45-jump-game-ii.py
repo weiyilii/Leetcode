ce97a5cd09a9408ec9976e80d2721e8a3a7e0dc6
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-25 18:35:23 -0400

    Time: 358 ms (35.50%), Space: 15.1 MB (21.61%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,9 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        step, far, end = 0, 0, 0
-        for i in range(len(nums) - 1):
-            far = max(far, i + nums[i])
-            if i == end:
-                step += 1
-                end = far
-        return step
+        n = len(nums)
+        if n == 1:
+            return 0
+        
+        i, level = 0, 0
+        cur_max, next_max = 0, 0
+        while i <= cur_max:
+            for j in range(i, cur_max + 1):
+                next_max = max(next_max, j + nums[j])
+                if next_max >= n-1:
+                    return level + 1
+            i = cur_max
+            cur_max = next_max
+            level += 1
+        return -1
