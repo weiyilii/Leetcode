a057943813564b5b6cbae5b10d4a73b2447a9691
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-25 18:22:28 -0400

    Time: 277 ms (48.69%), Space: 15.1 MB (21.61%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def jump(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        jumps, currend, far = 0, 0, 0
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        step, far, end = 0, 0, 0
         for i in range(len(nums) - 1):
             far = max(far, i + nums[i])
-            if i == currend:
-                jumps += 1
-                currend = far
-        return jumps
+            if i == end:
+                step += 1
+                end = far
+        return step
