4b3a90ef6a95af408ffed39a889a78d3f5f4895e
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-25 19:04:47 -0400

    Time: 5180 ms (5.00%), Space: 37.4 MB (6.67%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxResult(self, nums: List[int], k: int) -> int:
+        from heapq import heappop, heappush
+        n = len(nums)
+        dp = [0]*n
+        dp[-1] = nums[-1]
+        h = [(-dp[-1], n-1)]
+        for i in range(n-2, -1, -1):
+            val, pos = heappop(h)
+            while pos > i+k:
+                val, pos = heappop(h)
+            heappush(h, (val, pos))
+            dp[i] = nums[i] - val
+            heappush(h, (-dp[i], i))
+        return dp[0]
