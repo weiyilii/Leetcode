53ddfe4e118785631f2c28c0a72aea7be72763d9
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-26 23:30:21 -0400

    Time: 185 ms (32.41%), Space: 15.5 MB (71.58%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -2,10 +2,12 @@ class Solution:
     def calculate(self, s: str) -> int:
         stack = [0]
         num, op = 0, "+"
-        for c in s:
+        for i in range(len(s)):
+            #print(stack)
+            c = s[i]
             if c.isdigit():
                 num = num*10 + int(c)
-            elif c in ("+", "-", "*", "/"):
+            if c in ("+", "-", "*", "/") or i == len(s)-1:
                 if op == "*":
                     prev = stack.pop()
                     new = prev*num
@@ -19,17 +21,4 @@ class Solution:
                 elif op == "-":
                     stack.append(-num)
                 num, op = 0, c
-                
-        if op == "*":
-            prev = stack.pop()
-            new = prev*num
-            stack.append(new)
-        elif op == "/":
-            prev = stack.pop()
-            new = int(prev/num)
-            stack.append(new)
-        elif op == "+":
-            stack.append(num)
-        elif op == "-":
-            stack.append(-num)
         return sum(stack)
