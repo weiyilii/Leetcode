4b89996ba09f2042260c16261e328cf85c2f1191
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-28 18:36:40 -0400

    Time: 653 ms (9.05%), Space: 0B (100.00%) - LeetHub

--- 626-exchange-seats/626-exchange-seats.sql
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -1,10 +1,8 @@
 # Write your MySQL query statement below
 SELECT CASE
-           WHEN MOD(ID, 2) = 0 THEN ID - 1 
-           WHEN MOD(ID, 2) != 0 AND ID != COUNTS THEN ID + 1
+           WHEN MOD(ID, 2) = 0 THEN ID-1
+           WHEN MOD(ID, 2) = 1 AND ID < (SELECT MAX(ID) FROM SEAT) THEN ID+1
            ELSE ID END AS ID,
        STUDENT
-FROM SEAT, 
-(SELECT COUNT(*) AS COUNTS
-FROM SEAT) AS SEAT_COUNTS
-ORDER BY ID ASC
+FROM SEAT
+ORDER BY ID
