a682747d69962f598431641203a9407f5c1ab326
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-28 00:08:48 -0400

    Update and rename 1541-minimum-insertions-to-balance-a-parentheses-string.py to 1541-minimum-insertions-to-balance-a-parentheses-string_1.py

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string_1.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string_1.py
@@ -1,5 +1,10 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        # similar to 921
+        # res records how many parenthese inserted through the process
+        # need equals to the number of right half needed dynamicly
+        # determine whether to insert according to insert
+        
         res, need = 0, 0
         for c in s:
             if c == "(":
