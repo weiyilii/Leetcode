ffd5a5932865e35cf31a75f84d7b347ef4952f75
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-27 22:58:36 -0400

    Time: 90 ms (17.64%), Space: 13.8 MB (94.35%) - LeetHub

--- 772-basic-calculator-iii/772-basic-calculator-iii.py
+++ 772-basic-calculator-iii/772-basic-calculator-iii.py
@@ -1,41 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        stack = []
-        num, op = 0, "+"
         
-        def compute(num, op):
-            if op == "+":
-                stack.append(num)
-            elif op == "-":
-                stack.append(-num)
-            elif op == "*":
-                prev = stack.pop()
-                stack.append(prev*num)
-            elif op == "/":
-                prev = stack.pop()
-                stack.append(int(float(prev)/num))
-        
-        for i in range(len(s)):
-            print(stack)
-            c = s[i]
-            if c.isdigit():
-                num = num*10 + int(c)
-            if c in ("+", "-", "*", "/"):
-                compute(num, op)
-                num, op = 0, c
-            if c == "(":
-                #stack.append(num)
-                stack.append(op)
-                num, op = 0, "+"
-            if c == ")":
-                compute(num, op)
-                num = 0
-                while stack and stack[-1] not in ("+", "-", "*", "/"):
-                    num += stack.pop()
-                if stack:
-                    op = stack.pop()
-                    compute(num, op)
+        def helper(s):
+            stack = []
+            num, op = 0, "+"
+            
+            while len(s) > 0:
+                c = s.popleft()
+                if c.isdigit():
+                    num = num*10 + int(c)
+                    
+                if c == "(":
+                    num = helper(s)
+                    
+                if (not c.isdigit() and c != " ") or len(s) == 0:
+                    if op == "+":
+                        stack.append(num)
+                    if op == "-":
+                        stack.append(-num)
+                    if op == "*":
+                        stack[-1] = stack[-1]*num
+                    if op == "/":
+                        stack[-1] = int(float(stack[-1])/num)
                     num, op = 0, c
-        compute(num, op)
-        return sum(stack)
                 
+                if c == ")":
+                    break
+            return sum(stack)
+        
+        return helper(collections.deque(s))
