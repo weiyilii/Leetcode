a64f6c73fecdda8ed9bbe7d66301eee4b8272276
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-27 23:50:28 -0400

    Update 921-minimum-add-to-make-parentheses-valid.py

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -1,5 +1,7 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
+        # res: how many left parenthes needed to be inserted
+        # need: how many right parenthese needed to close left half
         res, need = 0, 0
         for c in s:
             if c == "(":
@@ -8,5 +10,6 @@ class Solution:
                 need -= 1
             if need == -1:
                 res += 1
+                # Dont forget to reset need
                 need = 0
         return res + need
