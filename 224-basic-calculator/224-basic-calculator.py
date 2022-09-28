86e5076ccc66757143b97fdcf7445c123d61664a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-27 22:57:27 -0400

    Time: 308 ms (11.33%), Space: 16.5 MB (13.79%) - LeetHub

--- 224-basic-calculator/224-basic-calculator.py
+++ 224-basic-calculator/224-basic-calculator.py
@@ -1,25 +1,31 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        res, num, sign = 0, 0, 1
-        stack = []
-        for c in s:
-            if c.isdigit():
-                num = num*10 + int(c)
-            elif c == "+":
-                res += num*sign
-                num, sign = 0, 1
-            elif c == "-":
-                res += num*sign
-                num, sign = 0, -1
-            elif c == "(":
-                stack.append(res)
-                stack.append(sign)
-                res = 0
-                num, sign = 0, 1
-            elif c == ")":
-                res += num*sign
-                res = res*stack.pop()
-                res += stack.pop()
-                num, sign = 0, 1
         
-        return res + num*sign
+        def helper(s):
+            stack = []
+            num, op = 0, "+"
+            
+            while len(s) > 0:
+                c = s.popleft()
+                if c.isdigit():
+                    num = num*10 + int(c)
+                    
+                if c == "(":
+                    num = helper(s)
+                    
+                if (not c.isdigit() and c != " ") or len(s) == 0:
+                    if op == "+":
+                        stack.append(num)
+                    if op == "-":
+                        stack.append(-num)
+                    if op == "*":
+                        stack[-1] = stack[-1]*num
+                    if op == "/":
+                        stack[-1] = int(float(stack[-1])/num)
+                    num, op = 0, c
+                
+                if c == ")":
+                    break
+            return sum(stack)
+        
+        return helper(collections.deque(s))
