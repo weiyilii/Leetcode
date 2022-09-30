96e65717890d1ff1290a1e1989f6b8f0f1f79a8a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-29 23:52:13 -0400

    Time: 211 ms (12.75%), Space: 21.2 MB (94.33%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -13,9 +13,8 @@ class Solution:
                 return 0
             left = max(dfs(root.left), 0)
             right = max(dfs(root.right), 0)
-            self.res = max(self.res, root.val + left + right)
+            self.res = max(self.res, left + right + root.val)
             return root.val + max(left, right)
         
         dfs(root)
-        
         return self.res
