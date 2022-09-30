c4ce7b389bbadd49b5f3eec6b96b811c283a2f3b
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-29 21:08:29 -0400

    Time: 244 ms (29.02%), Space: 17.9 MB (52.16%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -5,14 +5,15 @@
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        if not lists:
-            return
-        if len(lists) == 1:
+        l = len(lists)
+        if l == 0:
+            return None
+        if l == 1:
             return lists[0]
-        left, right = 0, len(lists) - 1
-        mid = left + (right - left)//2
-        lefthalf = self.mergeKLists(lists[:mid+1])
-        righthalf = self.mergeKLists(lists[mid+1:])
+        left, right = 0, l - 1
+        mid = left + (right - left) // 2
+        lefthalf = self.mergeKLists(lists[left:mid + 1])
+        righthalf = self.mergeKLists(lists[mid+1:right+1])
         return self.mergeTwo(lefthalf, righthalf)
     
     def mergeTwo(self, l1, l2):
@@ -20,9 +21,15 @@ class Solution:
             return l2
         if not l2:
             return l1
-        if l1.val <= l2.val:
-            l1.next = self.mergeTwo(l1.next, l2)
-            return l1
-        else:
-            l2.next = self.mergeTwo(l1, l2.next)
-            return l2
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            x, y = l1.val, l2.val
+            if x <= y:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+        return dummy.next
