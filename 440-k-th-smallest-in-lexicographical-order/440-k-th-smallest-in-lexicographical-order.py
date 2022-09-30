6f06ed96d5ba3592f3bb1dc9c292859396e8a88a
Weiyi Li <92836670+wyliadrian@users.noreply.github.com>
2022-09-30 14:24:43 -0400

    Time: 62 ms (37.40%), Space: 13.9 MB (80.49%) - LeetHub

--- 440-k-th-smallest-in-lexicographical-order/440-k-th-smallest-in-lexicographical-order.py
+++ 440-k-th-smallest-in-lexicographical-order/440-k-th-smallest-in-lexicographical-order.py
@@ -1,18 +1,17 @@
 class Solution:
     def findKthNumber(self, n: int, k: int) -> int:
-        res = 1
         k -= 1
+        res = 1
         while k > 0:
+            interval = [res, res+1]
             count = 0
-            interval = [res, res + 1]
             while interval[0] <= n:
                 count += min(n+1, interval[1]) - interval[0]
                 interval = [interval[0]*10, interval[1]*10]
-            
-            if k >= count:
+            if count > k:
+                k -= 1
+                res *= 10
+            else:
                 k -= count
                 res += 1
-            else:
-                k -= 1
-                res = res*10
         return res
